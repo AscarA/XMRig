e63d63f0641e3e8b7051c774ea92557e8f1314eb
AscarA <allasc@mail.ru>
2023-09-10 17:54:49 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,5 +1,7 @@
 #bash /home/job/turbo-boost.sh enable
 #cpupower frequency-set -g performance
 cd xmrig/build
-name=$1
-./xmrig -o ssl://xmr-asia1.nanopool.org:10343 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8.${#name}/allasc@mail.ru -p x --randomx-1gb-pages --cpu-priority=5
+var1="46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8."
+var1+=$1
+var1+="/allasc@mail.ru"
+./xmrig -o ssl://xmr-asia1.nanopool.org:10343 -u $var1 -p x --randomx-1gb-pages --cpu-priority=5
