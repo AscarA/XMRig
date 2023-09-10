d385032aa58cbf212b11cd106fd13e92bb68b101
AscarA <allasc@mail.ru>
2023-09-10 18:44:59 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -4,4 +4,4 @@ cd xmrig/build
 var1="46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8."
 var1+=$1
 var1+="/allasc@mail.ru"
-./xmrig -o ssl://xmr-asia1.nanopool.org:10343 -u $var1 -p x --randomx-1gb-pages --cpu-priority=5 -a rx/0
+./xmrig -o stratum+ssl://xmr-asia1.nanopool.org:10343 -u $var1 -p x --randomx-1gb-pages --cpu-priority=5 -a rx/0
