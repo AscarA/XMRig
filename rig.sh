8237885dc96c9d8073c88f1dc5dd57d329fcc833
AscarA <allasc@mail.ru>
2023-09-12 13:59:15 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,4 +1,4 @@
 #bash /home/job/turbo-boost.sh enable
 #cpupower frequency-set -g performance
 cd xmrig/build
-./xmrig --url pool.hashvault.pro:80 --user 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 --pass $1 --donate-level 1  --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./xmrig --url stratum+ssl://pool.hashvault.pro:80 --user 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 --pass $1 --donate-level 1  --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
