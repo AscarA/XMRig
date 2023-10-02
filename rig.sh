198346c31c356dfbd7f0de086bc94dde9606c1f6
AscarA <allasc@mail.ru>
2023-10-02 15:00:35 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -3,6 +3,3 @@
 
 cd monero
 bash miner.sh --config=config.json
-
-#cd xmrig/build
-#./xmrig -o stratum+ssl://gulf.moneroocean.stream:20256 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 -p $1 --randomx-1gb-pages --cpu-priority=5
