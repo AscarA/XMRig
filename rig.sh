f4bf3ca86e395359596cf2ea00467e5a43887c96
AscarA <allasc@mail.ru>
2023-12-09 16:04:59 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,4 +1,2 @@
-#bash /home/job/turbo-boost.sh enable
-#cpupower frequency-set -g performance
 cd xmrig/build
-./xmrig -o stratum+ssl://gulf.moneroocean.stream:20256 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 -p $1 --randomx-1gb-pages --cpu-priority=5
+./xmrig -o stratum+tcp://randomx.mine.zergpool.com:4453 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 --donate-level 1 -p c=XMR,ID=$1 --tls --keepalive
