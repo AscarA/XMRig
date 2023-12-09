9f466aefc9da1f923e3d978b8db3478ba1dc20a3
AscarA <allasc@mail.ru>
2023-12-09 16:53:31 +0300

    Update rig_zergpool.sh

--- rig_zergpool.sh
+++ rig_zergpool.sh
@@ -1,3 +1,3 @@
 cd xmrig/build
-./xmrig -o stratum+tcp://randomx.mine.zergpool.com:4453 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 -p c=XMR,ID=$1 --tls --keepalive --randomx-1gb-pages --cpu-priority=5 --algo rx/0
+./xmrig -o stratum+ssl://randomx.asia.mine.zergpool.com:4453 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 --tls -p c=XMR,mc=XMR,ID=$1 --keepalive --randomx-1gb-pages --cpu-priority=5 --algo rx/0
 
