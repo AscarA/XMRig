ba5649b568915f6024d6b896284707ee30db7fd2
AscarA <allasc@mail.ru>
2024-01-18 16:06:23 +0300

    Update rig_hashvault_ZEPH.sh

--- rig_hashvault_ZEPH.sh
+++ rig_hashvault_ZEPH.sh
@@ -1,1 +1,4 @@
-xmrig --url pool.hashvault.pro:80 --user ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+#bash /home/job/turbo-boost.sh enable
+#cpupower frequency-set -g performance
+cd xmrig/build
+./xmrig --url pool.hashvault.pro:80 --user ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 --pass $1 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --randomx-1gb-pages --cpu-priority=5
