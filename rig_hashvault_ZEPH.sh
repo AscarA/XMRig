88795866806bfa0b0848c32fae4e5819071ed12c
AscarA <allasc@mail.ru>
2024-01-18 16:05:06 +0300

    Create rig_hashvault_ZEPH.sh

---
+++ rig_hashvault_ZEPH.sh
@@ -0,0 +1,1 @@
+xmrig --url pool.hashvault.pro:80 --user ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
