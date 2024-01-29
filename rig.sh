c3fdfef9c00cc226db7c2472c19d33ad6527b5e5
AscarA <allasc@mail.ru>
2024-01-29 12:24:23 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,4 +1,4 @@
 #bash /home/job/turbo-boost.sh enable
 #cpupower frequency-set -g performance
 cd xmrig/build
-./xmrig -o stratum+ssl://gulf.moneroocean.stream:20256 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 -p $1 --randomx-1gb-pages --cpu-priority=5 --donate-level 1
+./xmrig -o stratum+ssl://gulf.moneroocean.stream:20256 -u 46RY7TncQG8DguotGBJRyrAsiCfZH71msD8nXuECLfPFNc58rWQvL17S5tnRN43PFbN7Q1S4ksy5o9QVny1k5KkjFUFQEu8 -p $1 --randomx-1gb-pages --cpu-priority=5 --donate-level 1 --opencl
