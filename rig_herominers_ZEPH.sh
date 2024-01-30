dbc1ed7b167f0edf4a9b3e24095c45134c319c92
AscarA <allasc@mail.ru>
2024-01-30 12:54:18 +0300

    Update rig_herominers_ZEPH.sh

--- rig_herominers_ZEPH.sh
+++ rig_herominers_ZEPH.sh
@@ -1,1 +1,4 @@
-
+#bash /home/job/turbo-boost.sh enable
+#cpupower frequency-set -g performance
+cd xmrig/build
+./xmrig -o kr.zephyr.herominers.com:1123 -u ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 -p $1 -a rx/0 -k --donate-level 1 --randomx-1gb-pages --cpu-priority=5
