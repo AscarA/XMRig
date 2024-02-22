c895905fae6631f85faa9482fde8722f4f825574
AscarA <allasc@mail.ru>
2024-02-22 18:03:16 +0300

    Update rig_local.sh

--- rig_local.sh
+++ rig_local.sh
@@ -1,1 +1,4 @@
-nfgnfg
+#bash /home/job/turbo-boost.sh enable
+#cpupower frequency-set -g performance
+cd xmrig/build
+./xmrig -o 192.168.8.252:33333 -u ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 -p $1 -a rx/0 -k --donate-level 1 --randomx-1gb-pages --cpu-priority=5
