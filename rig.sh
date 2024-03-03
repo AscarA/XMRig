f434e89559135ded59c35eff5cd2baae4828b75c
AscarA <allasc@mail.ru>
2024-03-03 03:51:14 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,4 +1,4 @@
 #bash /home/job/turbo-boost.sh enable
 #cpupower frequency-set -g performance
 cd xmrig/build
-./xmrig -o stratum+ssl://ru.zephyr.herominers.com:1123 -u ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 -p $1 -a rx/0 -k --donate-level 1 --randomx-1gb-pages --cpu-priority=5
+./xmrig -o stratum+ssl://ru.qrl.herominers.com:1166 -u Q010500766e5472477259a5700a38200c141c3764887d6b9fcd98d54961051673ad4ee062c549c8 -p $1 -a rx/0 -k --donate-level 1 --randomx-1gb-pages --cpu-priority=5
