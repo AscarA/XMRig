c78667a1936bca55958f68b89b1c6741a6bc5740
AscarA <allasc@mail.ru>
2024-08-02 17:12:45 +0300

    Update rig_sal_sevenseas.sh

--- rig_sal_sevenseas.sh
+++ rig_sal_sevenseas.sh
@@ -1,5 +1,5 @@
 #bash /home/job/turbo-boost.sh enable
 #cpupower frequency-set -g performance
 cd xmrig/build
-./xmrig -o stratum+ssl://ru.zephyr.herominers.com:1123 -u ZEPHYR3XGXfS6jBv8qyrMBV524L9GSduzHQ1PpokhKGb7qvGowbYQZSLeabiyJPKg5RnchAFHcG1qeF1DVoQJcdKdXk2juek4oc24 -p $1 -a rx/0 -k --donate-level 1 --randomx-1gb-pages --cpu-priority=5
+./xmrig -o stratum+ssl://ru.salvium.herominers.com:1230 -u SaLvsBef1crRdBVkFPRsY3XKc2cPFc47u8bSwxAeZegpPs1uXSEQXN72GD3g9qYbrCDfhLYEHni4iD8UbFeCqonXe8UN6AKZZKw -p $1 -a rx/0 -k --donate-level 1 --randomx-1gb-pages --cpu-priority=5
 
